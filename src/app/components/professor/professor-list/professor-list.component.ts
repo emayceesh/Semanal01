65da9861af5e1a7d93f44288d0d875d98693ec85
DanleyFernando <160700716+DanleyFernando@users.noreply.github.com>
2025-03-23 11:49:44 -0300

    coisa boa e fazer  rs preciso dajuda

--- src/app/components/professor/professor-list/professor-list.component.ts
+++ src/app/components/professor/professor-list/professor-list.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { Professor } from '../../../models/professor';
 
 @Component({
   selector: 'app-professor-list',
   standalone: true,
   imports: [],
   templateUrl: './professor-list.component.html',
-  styleUrl: './professor-list.component.scss'
+  styleUrl: './professor-list.component.scss',
 })
 export class ProfessorListComponent {
+  lista: Professor[] = [];
 
+  constructor() {
+    this.findAll();
+  }
+
+  findAll(){
+    let professor1 = new Professor();
+    professor1.id = 1;
+    professor1.nomeProfessor = 'Albert Einstein';
+    professor1.cpf = '111.222.333-45';
+    professor1.email = 'einstein@universidade.com';
+    professor1.especialidade = 'Relatividade';
+    
+    let professor2 = new Professor();
+    professor2.id = 2;
+    professor2.nomeProfessor = 'Isaac Newton';
+    professor2.cpf = '222.333.444-56';
+    professor2.email = 'newton@universidade.com';
+    professor2.especialidade = 'Cálculo e Gravidade';
+    
+    let professor3 = new Professor();
+    professor3.id = 3;
+    professor3.nomeProfessor = 'Marie Curie';
+    professor3.cpf = '333.444.555-67';
+    professor3.email = 'curie@universidade.com';
+    professor3.especialidade = 'Radioatividade';
+
+    this.lista.push(professor1, professor2, professor3);
+  }
+
+  delete(professor: Professor){
+    let indice = this.lista.findIndex(x => {return x.id == professor.id});
+    if(confirm('Professor removido com sucesso!')){
+      this.lista.splice(indice, 1); 
+    }
+  }
 }
--- src/app/components/turma/turma-list/turma-list.component.ts
+++ src/app/components/turma/turma-list/turma-list.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { Turma } from '../../../models/turma';
 
 @Component({
   selector: 'app-turma-list',
   standalone: true,
   imports: [],
   templateUrl: './turma-list.component.html',
-  styleUrl: './turma-list.component.scss'
+  styleUrl: './turma-list.component.scss',
 })
 export class TurmaListComponent {
+  lista: Turma[] = [];
 
+  constructor() {
+    this.findAll();
+  }
+
+  findAll(){
+    let turma1 = new Turma();
+    turma1.id = 1;
+    turma1.nomeTurma = 'Turma A';
+    turma1.semestre = '1º';
+    turma1.anoTurma = '2024';
+    turma1.turno = 'Matutino';
+    
+    let turma2 = new Turma();
+    turma2.id = 2;
+    turma2.nomeTurma = 'Turma B';
+    turma2.semestre = '2º';
+    turma2.anoTurma = '2024';
+    turma2.turno = 'Vespertino';
+    
+    let turma3 = new Turma();
+    turma3.id = 3;
+    turma3.nomeTurma = 'Turma C';
+    turma3.semestre = '1º';
+    turma3.anoTurma = '2025';
+    turma3.turno = 'Noturno';
+
+    this.lista.push(turma1, turma2, turma3);
+  }
+
+  delete(turma: Turma){
+    let indice = this.lista.findIndex(x => {return x.id == turma.id});
+    if(confirm('Turma removida com sucesso!')){
+      this.lista.splice(indice, 1); //deletando um objeto na posição INDICE
+    }
+  }
 }

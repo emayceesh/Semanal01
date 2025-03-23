3fd8664eb6fbd0dff22f2d227e2f34a5819251bd
DanleyFernando <160700716+DanleyFernando@users.noreply.github.com>
2025-03-23 11:43:55 -0300

    Subindo a boa

--- src/app/components/professor/professor-form/professor-form.component.ts
+++ src/app/components/professor/professor-form/professor-form.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Professor } from '../../../models/professor';
 
 @Component({
   selector: 'app-professor-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MdbFormsModule],
   templateUrl: './professor-form.component.html',
-  styleUrl: './professor-form.component.scss'
+  styleUrl: './professor-form.component.scss',
 })
 export class ProfessorFormComponent {
+  professor = new Professor();
 
+  rotaAtivida = inject(ActivatedRoute);
+
+  constructor() {
+    let id = this.rotaAtivida.snapshot.params['id'];
+    if (id) {
+      
+
+      let professor1 = new Professor();
+      professor1.id = 1;
+      professor1.nomeProfessor = 'sexasa';
+      professor1.cpf = '123.456.789-00';
+      professor1.email = 'haxieixersidade.com';
+      professor1.especialidade = 'Relatividade';
+      this.professor = professor1; 
+    }
+  }
+
+  save() {
+    if (this.professor.id > 0) {
+      // UPDATE
+      alert('Professor alterado com sucesso!');
+    } else {
+      // SAVE
+      alert('Professor cadastrado com sucesso!');
+    }
+  }
 }
--- src/app/components/turma/turma-form/turma-form.component.ts
+++ src/app/components/turma/turma-form/turma-form.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Turma } from '../../../models/turma';
 
 @Component({
   selector: 'app-turma-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MdbFormsModule],
   templateUrl: './turma-form.component.html',
-  styleUrl: './turma-form.component.scss'
+  styleUrl: './turma-form.component.scss',
 })
 export class TurmaFormComponent {
+  turma = new Turma();
 
+  rotaAtivida = inject(ActivatedRoute);
+
+  constructor() {
+    let id = this.rotaAtivida.snapshot.params['id'];
+    if (id) {
+      //AQUI VC VAI CHAMAR O FINDBYID()
+
+      let turma1 = new Turma();
+      turma1.id = 1;
+      turma1.nomeTurma = 'Turma A';
+      turma1.semestre = '1º Semestre';
+      turma1.anoTurma = '2024';
+      turma1.turno = 'Matutino';
+      this.turma = turma1; //setar o objeto no formulário
+    }
+  }
+
+  save() {
+    if (this.turma.id > 0) {
+      // UPDATE
+      alert('Turma alterada com sucesso!');
+    } else {
+      // SAVE
+      alert('Turma cadastrada com sucesso!');
+    }
+  }
 }
